31aaa535857cec15d65367248a909a07392be07a
LR32768 <lr200898@126.com>
2017-07-19 18:29:00 +0800

    Update feature.py

--- feature.py
+++ feature.py
@@ -1,6 +1,10 @@
 from imgproc import *
-mat = matget("0.jpg")
-feature = 10 * [0.0]
+import cv2
+import numpy as np
+
+mat = matget('2.jpg')
+img = imgread('2.jpg')
+feature = 20 * [0.0]
 
 
 def dot(v1, v2):
@@ -16,7 +20,7 @@ def uni(v):
 
 px = []
 py = []
-n = 0p
+n = 0
 
 for i in range(mat.shape[0]):
     for j in range(mat.shape[1]):
@@ -25,11 +29,12 @@ for i in range(mat.shape[0]):
             py.append(j)
             n = n + 1
 
-#Now strech feature 1;
+#Now strech feature 1(convexarea / rectanglearea);
 xmax = 0
 ymax = 0
 xmin = mat.shape[0]
 ymin = mat.shape[1]
+
 for i in range(n):
     #print("The coordinate",px[i],' ',py[i]);
     if px[i] < xmin:
@@ -44,68 +49,43 @@ for i in range(n):
 height = xmax - xmin
 width = ymax - ymin
 
+print(height)
+print(width)
+
 area = height * width
-l = convexhull(mat)
-convexlist = trans3(l)
-
-convx = []
-convy = []
-
-st = 0
-for i in range(len(convexlist)):
-    if (convexlist[i][0] > convexlist[st][0] or
-        (convexlist[i][0] == convexlist[st][0] and
-         convexlist[i][1] > convexlist[st][1])):
-        st = i
-convx.append(convexlist[st][0])
-convy.append(convexlist[st][1])
-#find the most right down point which must be one of the point of the convex hull
-
-cur = st
-curdir = [1, 0]
-curdot = 2
-nstep = 0
-
-for i in range(len(convexlist)):
-    if (i != st):
-        newdir = [
-            convexlist[i][0] - convexlist[st][0],
-            convexlist[i][1] - convexlist[st][1]
-        ]
-        newdot = dot(uni(curdir), uni(newdir))
-        if (newdot > 0 and newdot < curdot):
-            nstep = i
-            curdot = newdot
-curdir = [
-    convexlist[nstep][0] - convexlist[cur][0],
-    convexlist[nstep][1] - convexlist[cur][1]
-]
-cur = nstep
-
-while (cur != st):
-    convx.append(convexlist[cur][0])
-    convy.append(convexlist[cur][1])
-    curdot = 2
-    nstep = 0
-    for i in range(len(convexlist)):
-        if (i != cur):
-            newdir = [
-                convexlist[i][0] - convexlist[cur][0],
-                convexlist[i][1] - convexlist[cur][1]
-            ]
-            newdot = dot(uni(curdir), uni(newdir))
-            if (newdot > 0 and newdot < curdot):
-                nstep = i
-                curdot = newdot
-    curdir = [
-        convexlist[nstep][0] - convexlist[cur][0],
-        convexlist[nstep][1] - convexlist[cur][1]
-    ]
-    cur = nstep
-
-print(convx)
-print(convy)
+print(area)
 
 # To get the area of convex hull
+l = wholehull(img)
+print(l)
+conarea = 0.0
 
-# Now stech feature 2;
+l.append(l[0])
+for i in range(len(l)-1):
+   conarea = conarea + l[i][0] * l[i + 1][1] - l[i][1] * l[i + 1][0]
+
+conarea = conarea / 2.0
+
+print(conarea)
+feature[0] = conarea / area
+
+
+# Now strech feature 2, 3;(Minimum deviation line slope)
+line = findaxis(img)
+print(line)
+
+feature[2] = line[0]
+feature[3] = line[1] / height
+
+#Now stretch feature 4; (Gravity center)
+Gravitycenter = findcenter(mat)
+feature[4] = Gravitycenter[0]
+feature[5] = Gravitycenter[1]
+
+#Now strtch feature 5; (White ratio)
+blackblocks = len(px)
+feature[5] = blackblocks / conarea
+
+print(feature)
+
+
